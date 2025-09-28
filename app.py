3a8508a9d3ab9b4d9a22fbac3e9c1ae6957b1c2a
monkeyproduction98-boop <monkeyproduction98@gmail.com>
2025-09-28 04:03:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ st.title("Choose an Option")
 col1, col2 = st.columns(2)
 
 with col1:
+    if st.button(""):
+        st.switch_page("pages/1_Calculator.py")
     st.markdown(
         """
-        <a href="/dtf_dashboard" target="_self">
-            <img src="https://img.freepik.com/free-vector/calculator-floating-cartoon-vector-icon-illustration-finance-business-icon-concept-isolated-flat_138676-9297.jpg" 
-            style="width:100%; border-radius:15px;">
-        </a>
+        <img src="https://img.freepik.com/free-vector/calculator-floating-cartoon-vector-icon-illustration-finance-business-icon-concept-isolated-flat_138676-9297.jpg" 
+        style="width:100%; border-radius:15px; cursor:pointer;">
         """,
         unsafe_allow_html=True
     )
 
 with col2:
+    if st.button(" "):
+        st.switch_page("pages/2_Reports.py")
     st.markdown(
         """
-        <a href="/reports" target="_self">
-            <img src="https://holistiquetraining.com/storage/write-a-report.jpg" 
-            style="width:100%; border-radius:15px;">
-        </a>
+        <img src="https://holistiquetraining.com/storage/write-a-report.jpg" 
+        style="width:100%; border-radius:15px; cursor:pointer;">
         """,
         unsafe_allow_html=True
     )
